b5b028774d0beb90b592ce97d3d4c245b55b2212
Cocojarukami <cocojarukami2@gmail.com>
2024-11-09 15:54:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const canvas = document.getElementById("canvas");
-    const gridSize = 20; // Μέγεθος 20x20
+    const gridSize = 20;
 
+    // Δημιουργία κελιών για το grid
     if (canvas) {
-        // Δημιουργία κελιών στο grid
         for (let i = 0; i < gridSize * gridSize; i++) {
             const cell = document.createElement("div");
             cell.classList.add("cell");
